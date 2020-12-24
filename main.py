42c69491a7aa87d3ecc90a8736aadce1ba1e1e7d
ramon.rotaeche <ramonrotaeche@gmail.com>
2020-12-24 17:46:29 +0100

    Más papers, requirements, nueva carpeta

--- main.py
+++ main.py
@@ -4,13 +4,28 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def ejecutar_algoritmo(n_iter, tam_pop):
+    '''
+    Función principal para ejecutar todos los pasos e iteraciones del algoritmo evolutivo. No devuelve ningún objeto.
+    :param n_iter: número de iteraciones
+    :param tam_pop: tamaño de la población
+    '''
+    inicializar_población()
+    for i in range(n_iter + 1):
+        for ind in range(tam_pop + 1):
+            calcular_fit() # Calculamos el valor de fitness para cada individuo
+
+        seleccionar_padres()
+        recombinar_mutar()
+        seleccionar_supervivientes()
+
+    seleccionar_solución()
+
+
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    ejecutar_algoritmo(100, 30)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
